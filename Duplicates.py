8e8ca51c1cccd17f198e7b61981b56aa0d775998
Prathik Kini <kiniprathik@yahoo.com>
2022-08-15 13:04:49 +0530

    first commit

---
+++ Duplicates.py
@@ -0,0 +1,12 @@
+def duplicates(nums):
+    hashset = set()
+    for n in nums:
+        if n in hashset:
+            return True
+        hashset.add(n)
+    return False
+
+print(duplicates([1,2,3,4,5,1]))
+
+
+
